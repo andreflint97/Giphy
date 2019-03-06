fab6c7b9d337ce2961c6d5f0a489016592e260b8
andre flint <andreflint97@icloud.com>
2019-03-06 00:16:54 -0500

    changes

---
+++ assets/app.js
@@ -0,0 +1,60 @@
+var athletesArray = [
+    "Dennis Rodman",
+    "Baker Mayfield",
+    "Bo Jackson",
+    "Jon Jones",
+    ]
+
+$(document).ready(function() {
+    for (var i = 0; i < athletesArray.length; i++) {
+        $("#ath-buttons").append("<button type='button' onclick='searchGif(\"" + athletesArray[i] + "\")' class='btn btn-primary' value=' " + athletesArray[i] + "'> " + athletesArray[i] + " </button>");
+    }
+});
+
+function athleteButtonClicked() {
+    var userInput = $('#ath-input').val();
+    searchGif(userInput);
+}
+
+function submitButtonClicked() {
+    var userInput = $('#ath-input').val();
+
+    if (userInput) {
+        $('#ath-buttons').append("<button type='button' onclick='searchGif(\"" + userInput + "\")' class='btn btn-primary' value=' " + userInput + "'> " + userInput + " </button>");
+    }
+}
+
+function searchGif(gifName) {
+    $.ajax({
+        url: 'https://api.giphy.com/v1/gifs/search?q= ' + gifName + ' &api_key=qO2AsDAqVZKmNRW5mvXtHw8UjTut5Ym7',
+        type: 'GET',
+        })
+        .done(function(response) {
+            displayGif(response);
+        })
+}
+
+function displayGif(response) {
+    $('#athletes').empty();
+    for (var i = 0; i < response.data.length; i++) {
+        var rating = "<div class='ratings'> Rating:  " + (response.data[i].rating) + " </div>";
+        var image = rating + '<img src= " ' + response.data[i].images.fixed_height_still.url +
+            '" data-still=" ' + response.data[i].images.fixed_height_still.url +
+            ' " data-animate=" ' + response.data[i].images.fixed_height.url + '" data-state="still" class="movImage" style= "width:250px; height:250px">';
+
+        image = '<div class="col-md-4">' + image + "</div>";
+        $('#athletes').append(image);
+    }
+
+    $('.movImage').on('click', function() {
+        var state = $(this).attr('data-state');
+        if (state == 'still') {
+            $(this).attr('src', $(this).attr("data-animate"));
+            $(this).attr('data-state', 'animate');
+        } else {
+            $(this).attr('src', $(this).attr("data-still"));
+            $(this).attr('data-state', 'still');
+        }
+
+    });
+}
